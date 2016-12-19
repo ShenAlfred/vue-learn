1670faf9f637572e76551ac88428b93c5b7fef80
shenyuanchen <shenyuanchen@mail.xusenintl.com>
2016-12-19 18:33:42 +0800

    'learn'

--- src/js/index.js
+++ src/js/index.js
@@ -53,3 +53,58 @@ vm.$watch('a', function(newVal, oldVal) {
     console.log("旧的值:" + oldVal);
     console.log(this);
 });
+
+
+var todo = new Vue({
+   el: "#todo-example",
+    data: {
+        newtodo: '',
+        todos: [
+            "new item1",
+            "new item2",
+            "new item3"
+        ]
+    },
+    methods: {
+       addNewTodo: function() {
+           this.todos.push(this.newtodo);
+           this.newtodo = '';
+       },
+       remove: function(index) {
+         this.todos.splice(index, 1)
+       }
+    }
+});
+
+var click_example = new Vue({
+    el: '#example-1',
+    data: {
+        counter: 0
+    }
+});
+
+var input_checks = new Vue({
+    el: "#student",
+    data: {
+        checkedNames : []
+    }
+});
+
+Vue.component('my-component', {
+   template: "<h1>{{text}}</h1>",
+    data: function() {
+       return  {
+           text: 'Hello Vue'
+       }
+    }
+});
+
+Vue.component("props-component", {
+    template: '<span>props is {{ message }}</span>',
+    props: ['message']
+});
+
+var components = new Vue({
+    el: "#components"
+});
+
